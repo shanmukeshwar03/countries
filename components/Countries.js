106268098492b0a004b3957b1a6b59ff6897d6bb
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-12 11:44:38 +0530

    Update countries

--- components/Countries.js
+++ components/Countries.js
@@ -1,32 +1,32 @@
-import countries from 'utils/countries'
-import commanumber from 'comma-number'
-import router from 'next/router'
+import countries from "utils/countries";
+import commanumber from "comma-number";
+import router from "next/router";
 
 const Countries = ({ query }) => {
   return (
     <div className="countries__container">
       {countries.map((country, key) => {
-        if (!query || country.name.toLowerCase().includes(query))
+        if (!query || country.name.common.toLowerCase().includes(query))
           return (
             <div
               className="countries__country"
               key={key}
               onClick={() => {
-                router.push(`/${country.name}?index=${key}`)
+                router.push(`/${country.name.common}?index=${key}`);
               }}
             >
-              <img src={country.flag} />
+              <img src={country.flags.svg} />
               <div>
-                <span>{country.name}</span>
+                <span>{country.name.common}</span>
                 <span>Population : {commanumber(country.population)}</span>
                 <span>Region : {country.region}</span>
                 <span>Capital : {country.capital}</span>
               </div>
             </div>
-          )
+          );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Countries
+export default Countries;
--- pages/[country].js
+++ pages/[country].js
@@ -1,83 +1,96 @@
-import commaNumber from 'comma-number'
-import countries from 'utils/countries'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import commaNumber from "comma-number";
+import countries from "utils/countries";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-const insertCommas = (arr, args) => {
-  let data = ''
+const insertCommas = (arr) => {
+  let data = "";
   arr.forEach((element) => {
-    if (args) data += element[args] + ' , '
-    else data += element + ' , '
-  })
-  data = data.slice(0, data.length - 2)
-  return data
-}
+    data += element + " , ";
+  });
+  data = data.slice(0, data.length - 2);
+  return data;
+};
 
 const Country = () => {
-  const [country, setcountry] = useState(undefined)
-  const router = useRouter()
+  const [country, setcountry] = useState(undefined);
+  const router = useRouter();
   useEffect(() => {
-    setcountry(countries[router.query.index])
-  }, [])
+    setcountry(countries[router.query.index]);
+  }, []);
 
   if (!country) {
-    return <span className='center'>Something went wrong</span>
+    return <span className="center">Something went wrong</span>;
   }
 
   return (
     <div className="country__container">
-      <img src={country.flag} />
+      <img src={country.flags.svg} />
       <div>
-        <span className="country__title">{country.name}</span>
+        <span className="country__title">{country.name.common}</span>
         <div className="country__details">
           <span>
-            <span className="span__title">Native Name</span> :{' '}
-            <span className="span__value">{country.nativeName}</span>
+            <span className="span__title">Official</span> :{" "}
+            <span className="span__value">{country.name.official}</span>
           </span>
           <span>
-            <span className="span__title">Top Level Domain</span> :{' '}
+            <span className="span__title">Independent Country</span> :{" "}
             <span className="span__value">
-              {insertCommas(country.topLevelDomain)}
+              {country.independent.toString()}
             </span>
           </span>
           <span>
-            <span className="span__title">Population</span> :{' '}
+            <span className="span__title">Top Level Domain</span> :{" "}
+            <span className="span__value">{insertCommas(country.tld)}</span>
+          </span>
+          <span>
+            <span className="span__title">Population</span> :{" "}
             <span className="span__value">
               {commaNumber(country.population)}
             </span>
           </span>
           <span>
-            <span className="span__title">Currencies</span> :{' '}
+            <span className="span__title">Currencies</span> :{" "}
             <span className="span__value">
-              {insertCommas(country.languages, 'name')}
+              {country.currencies[Object.keys(country.currencies)[0]]["name"]}
+              {` (${
+                country.currencies[Object.keys(country.currencies)[0]]["symbol"]
+              })`}
             </span>
           </span>
           <span>
-            <span className="span__title">Region</span> :{' '}
+            <span className="span__title">Region</span> :{" "}
             <span className="span__value">{country.region}</span>
           </span>
           <span>
-            <span className="span__title">Sub Region</span> :{' '}
+            <span className="span__title">Sub Region</span> :{" "}
             <span className="span__value">{country.subregion}</span>
           </span>
           <span>
-            <span className="span__title">Languages</span> :{' '}
+            <span className="span__title">Languages</span> :{" "}
             <span className="span__value">
-              {insertCommas(country.languages, 'name')}
+              {insertCommas(Object.values(country.languages))}
             </span>
           </span>
           <span>
-            <span className="span__title">Capital</span> :{' '}
+            <span className="span__title">Capital</span> :{" "}
             <span className="span__value">{country.capital}</span>
           </span>
           <span>
-            <span className="span__title">border countries</span> :{' '}
+            <span className="span__title">View in maps</span> :
+            <span className="span__value">
+              {<Link href={country.maps.googleMaps}> Google Maps</Link>}
+            </span>
+          </span>
+          <span>
+            <span className="span__title">border countries</span> :{" "}
             <span className="span__value">{insertCommas(country.borders)}</span>
           </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Country
+export default Country;
